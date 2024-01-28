5513dc687829be8b5c0430c9d6cfac1bc685ee50
rajneeshkumar1507 <rk1441933@gmail.com>
2024-01-28 22:08:39 +0530

    Number Sqaure Pattern

---
+++ Lecture007 Loops Extra Class Returns/number-square-pattern.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern1(int n){
+    for(int i = 1; i<=n;i++){
+        int count = i;
+        for(int j = 1; j<=n;j++){
+            cout<<count++;
+        }
+        cout<<endl;
+    }
+}
+
+/*
+    Do not other variable to solve the problem,
+    Simple doing, i+j-1 -> Gives you the increasing
+    counting
+*/
+void pattern2(int n){
+    for(int i = 1; i<=n;i++){
+        for(int j = 1; j<=n;j++){
+            cout<<(i+j-1);
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    pattern1(n);
+    cout<<endl;
+    pattern2(n);
+    return 0;
+}
