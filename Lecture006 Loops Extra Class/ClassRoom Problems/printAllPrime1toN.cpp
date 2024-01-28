25eaf2ea1c2e0e36bd8644ad36764d672798e0ee
rajneeshkumar1507 <rk1441933@gmail.com>
2024-01-28 23:55:49 +0530

    Checking with some other approach

--- Lecture006 Loops Extra Class/ClassRoom Problems/printAllPrime1toN.cpp
+++ Lecture006 Loops Extra Class/ClassRoom Problems/printAllPrime1toN.cpp
@@ -5,16 +5,13 @@ int isPrime(int n) {
     if (n <= 1) {
         return false;
     }
-
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
             return false;
         }
     }
-
     return true;
 }
-
 void primeNumber1toN(int n) {
     for (int i = 2; i <= n; i++) {
         if (isPrime(i)) {
